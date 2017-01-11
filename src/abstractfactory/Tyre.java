cb027f818afced416a1e3cc3222fb86b094b2c73
magic-cube <15619225472@163.com>
2017-01-11 22:13:13 +0800

    3

---
+++ src/abstractfactory/Tyre.java
@@ -0,0 +1,21 @@
+package abstractfactory;
+
+public interface Tyre {
+	void revolve();
+}
+class LuxuryTyre implements Tyre{
+
+	@Override
+	public void revolve() {
+		
+	}
+	
+}
+class LowTyre implements Tyre{
+
+	@Override
+	public void revolve() {
+		
+	}
+	
+}
